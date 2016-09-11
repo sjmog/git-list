72593a54a672b4a9042f72766892085f22c40698
Sam Morgan <s_morgan@me.com>
2016-09-11 12:06:48 +0100

    initial commit with unfactored script

---
+++ stats.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+git rev-list --reverse HEAD | 
+while read rev; do 
+  git log --oneline -1 $rev;  
+  git ls-tree -r $rev | 
+  awk {'print $3'} | 
+  xargs git show | 
+  wc -l; 
+done
