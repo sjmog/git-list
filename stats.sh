42b5d3d2067b2d98660c51767fe5613790fca389
Sam Morgan <s_morgan@me.com>
2016-09-11 12:14:26 +0100

    refactored script

--- stats.sh
+++ stats.sh
@@ -2,11 +2,29 @@
 
 set -e
 
-git rev-list --reverse HEAD | 
-while read rev; do 
-  git log --oneline -1 $rev;  
-  git ls-tree -r $rev | 
+filter=$1
+
+function main {
+  for commit in `commits`; do 
+    description  
+    line_count
+  done
+}
+
+function commits {
+  git rev-list --reverse HEAD
+}
+
+function description {
+  git log --oneline -1 $commit;
+}
+
+function line_count {
+  git ls-tree -r $commit | 
+  grep "$filter" |
   awk {'print $3'} | 
   xargs git show | 
   wc -l; 
-done
+}
+
+main
